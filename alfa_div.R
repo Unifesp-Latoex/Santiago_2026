f28a02d7dfac06c870450e498b4dade672afba34
latoex-unifesp <labtoxunifesp@gmail.com>
2026-08-18 09:19:06 -0300

    alfa_div.R

--- alfa_div.R
+++ alfa_div.R
@@ -28,20 +28,32 @@ head(metadata)
 shannon<-read_qza("shannon_vector.qza")
 shannon<-shannon$data %>% rownames_to_column("SampleID")
 gplots::venn(list(metadata=metadata$SampleID, shannon=shannon$SampleID))
+metadata <-
+  metadata %>%
+  left_join(shannon)
 
 obs_species<-read_qza("observed_features_vector.qza")
 obs_species <- obs_species$data %>% rownames_to_column("SampleID")
 gplots::venn(list(metadata=metadata$SampleID, obs_species=obs_species$SampleID))
+metadata <-
+  metadata %>%
+  left_join(obs_species)
 
 evenness<-read_qza("evenness_vector.qza")
 evenness <- evenness$data %>% rownames_to_column("SampleID")
 gplots::venn(list(metadata=metadata$SampleID, evenness=evenness$SampleID))
+metadata <-
+  metadata %>%
+  left_join(evenness)
 
 faithpd<-read_qza("faith_pd_vector.qza")
 faithpd <- faithpd$data %>% rownames_to_column("SampleID")
 faithpd <- faithpd %>%
   rename(index = SampleID, SampleID = V1, faith = V2)
 gplots::venn(list(metadata=metadata$SampleID, faithpd=faithpd$SampleID))
+metadata <-
+  metadata %>%
+  left_join(faithpd)
 
 
 # Set details
@@ -57,7 +69,7 @@ metadata$birth <- factor(metadata$birth, levels = c("VD", "VD-CF", "CS-CF"))
 metadata$time_point <- factor(metadata$time_point, levels = c("25", "53", "90"))
 
 
-metricas <- list(
+metrics <- list(
   shannon = list(col = "shannon_entropy", label = "Shannon Diversity"),
   faith   = list(col = "faith", label = "Faith PD"),
   pielou  = list(col = "pielou_evenness", label = "Pielou Evenness"),
@@ -67,19 +79,19 @@ metricas <- list(
 
 all_stats <- data.frame()
 
-for (metrica_name in names(metricas)) {
-  cat("Rodando métrica:", metrica_name, "\n")
+for (metrics_name in names(metrics)) {
+  cat("Metrics:", metrics_name, "\n")
   
-  metrica_col <- metricas[[metrica_name]]$col
-  metrica_label <- metricas[[metrica_name]]$label
-  if (!metrica_col %in% colnames(metadata)) {
-    stop(paste("Coluna", metrica_col, "não encontrada em metadata"))
+  metric_col <- metrics[[metrics_name]]$col
+  metric_label <- metrics[[metrics_name]]$label
+  if (!metric_col %in% colnames(metadata)) {
+    stop(paste("Column", metric_col, "not found in metadata"))
   }
 
   df_metric <- metadata %>%
-    dplyr::select(SampleID, birth, time_point, !!sym(metrica_col)) %>%
+    dplyr::select(SampleID, birth, time_point, !!sym(metric_col)) %>%
     drop_na()
-  dist_matrix <- dist(df_metric[[metrica_col]])
+  dist_matrix <- dist(df_metric[[metric_col]])
   dispersion_result <- betadisper(dist_matrix, group = df_metric$birth)
   dispersion_test <- permutest(dispersion_result)
   
@@ -91,7 +103,7 @@ for (metrica_name in names(metricas)) {
   pairwise_results <- data.frame()
   for (tp in unique(df_metric$time_point)) {
     df_tp <- df_metric %>% filter(time_point == tp)
-    dist_tp <- dist(df_tp[[metrica_col]])
+    dist_tp <- dist(df_tp[[metric_col]])
     pw <- pairwise.adonis(dist_tp, factors = df_tp$birth)
     pw$time_point <- tp
     pairwise_results <- rbind(pairwise_results, pw)
@@ -103,7 +115,7 @@ for (metrica_name in names(metricas)) {
     mutate(
       group1 = gsub(" vs .*", "", pairs),
       group2 = gsub(".* vs ", "", pairs),
-      y.position = max(df_metric[[metrica_col]], na.rm = TRUE) * 1.05,  # altura do bracket
+      y.position = max(df_metric[[metric_col]], na.rm = TRUE) * 1.05,  
       label = case_when(
         p.adjusted < 0.001 ~ "***",
         p.adjusted < 0.01  ~ "**",
@@ -114,14 +126,14 @@ for (metrica_name in names(metricas)) {
   
   
   stat_df <- data.frame(
-    Metric = metrica_label,
+    Metric = metric_label,
     PERMANOVA_p = permanova_result$`Pr(>F)`[1],
     pairwise_results
   )
   all_stats <- rbind(all_stats, stat_df)
   
  
-  p <- ggplot(df_metric, aes(x = time_point, y = !!sym(metrica_col), fill = birth)) +
+  p <- ggplot(df_metric, aes(x = time_point, y = !!sym(metric_col), fill = birth)) +
     geom_boxplot(alpha = 0.6, outlier.shape = NA,
                  color = "black", linewidth = 0.8,
                  position = position_dodge(width = 0.8)) +
@@ -129,7 +141,7 @@ for (metrica_name in names(metricas)) {
                 position = position_jitterdodge(jitter.width = 0.2, dodge.width = 0.8),
                 alpha = 0.8, size = 4) +
     scale_fill_manual(values = group.colors) +
-    labs(x = "Postnatal day", y = metrica_label, title = "") +
+    labs(x = "Postnatal day", y = metric_label, title = "") +
     theme_classic() +
     theme(
       legend.position = "bottom",
@@ -141,14 +153,14 @@ for (metrica_name in names(metricas)) {
       axis.line.x = element_line(size = 1.5),
       axis.line.y = element_line(size = 1.5)
     ) +
-    annotate("text", x = 1.5, y = min(df_metric[[metrica_col]], na.rm = TRUE) * 0.95,
+    annotate("text", x = 1.5, y = min(df_metric[[metric_col]], na.rm = TRUE) * 0.95,
              label = paste("PERMANOVA p =", signif(permanova_result$`Pr(>F)`[1], 3)),
              hjust = 0.5, size = 5, color = "black") +
     stat_pvalue_manual(sig_pairs, label = "label",
                        xmin = "group1", xmax = "group2",
                        y.position = "y.position", tip.length = 0.02)
 
-  ggsave(paste0("plot_", metrica_name, ".pdf"), plot = p, height = 6, width = 8)
+  ggsave(paste0("plot_", metrics_name, ".pdf"), plot = p, height = 6, width = 8)
 }
 
 write.xlsx(all_stats, "stats_all_metrics.xlsx", rowNames = FALSE)
